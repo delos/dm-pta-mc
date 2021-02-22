0561764a931a5eeb816ad46313b7879e94303809
Sten Delos <dsten@draco04.mpcdf.mpg.de>
2021-02-22 01:26:13 +0100

    sigma tool - clarify

--- tools/to_sigma.py
+++ tools/to_sigma.py
@@ -6,36 +6,44 @@ from glob import glob
 from os.path import basename
 from scipy.special import gammainc, erf, erfcinv
 
-# basic functions
+# sigma from p
 def sigma_from_p(p):
     return np.sqrt(2) * erfcinv(2*p)
+
+# p for earth calculation
 
 def p_from_snr_earth(snr, NP=np.inf):
     return 1. - gammainc(0.5, np.sqrt(2*(1 - 1./NP)) * snr)
 
-def p_from_snr_pulsar(snr, NP):
-    return 1. - erf(snr/np.sqrt(2))**NP
+# p for pulsar calculation
+
+
+def _p_from_snr_pulsar_s(snr, NP):
+  return 1. - erf(snr/np.sqrt(2))**NP
+def _p_from_snr_pulsar_l(snr, NP):
+  x = snr/np.sqrt(2)
+  return NP * np.exp(-x**2) / (x * np.sqrt(np.pi))
+def p_from_snr_pulsar(snr, N):
+  return np.select([snr<7.5,snr>=7.5],[
+                      _p_from_snr_pulsar_s(snr,N),
+                      _p_from_snr_pulsar_l(snr,N),
+                      ])
 
 # sigma for pulsar calculation  
 
-def sigma_p_s(snr,N):
+def _sigma_p_s(snr,N):
   return sigma_from_p(p_from_snr_pulsar(snr,N))
   
-def sigma_p_m(snr,N):
-  x = snr/np.sqrt(2)
-  return np.sqrt(2) * erfcinv(2*N * np.exp(-x**2) / (x * np.sqrt(np.pi)))
-
-def sigma_p_l(snr,N):
+def _sigma_p_l(snr,N):
   L1 = snr**2 + 2*np.log(snr/N)
   L2 = np.log(L1)
   W = L1 - L2 + L2/L1 + L2*(-2+L2)/(2*L1**2) + L2*(6-9*L2+2*L2**2)/(6*L1**3) + L2*(-12+36*L2-22*L2**2+3*L2**3)/(12*L1**4)
   return np.sqrt(W)
 
 def sigma_p(snr,N):
-  return np.piecewise(snr,[snr<7.5,(snr>=7.5)&(snr<=36),snr>36],[
-                      lambda s: sigma_p_s(s,N),
-                      lambda s: sigma_p_m(s,N),
-                      lambda s: sigma_p_l(s,N),
+  return np.piecewise(snr,[snr<=36,snr>36],[
+                      lambda s: _sigma_p_s(s,N),
+                      lambda s: _sigma_p_l(s,N),
                       ])
 
 # sigma for earth calculation
@@ -43,12 +51,13 @@ def sigma_p(snr,N):
 def sigma_e(snr,N):
   return sigma_from_p(p_from_snr_earth(snr, N))
 
+# ---
 
 if len(argv) < 2:
     raise ValueError("python script.py <directory>")
 
 with open(argv[1] + "/sigma.txt", "wt") as fp:
-    pass
+    fp.write("# outputfile , NP , SNR , sigma\n")
 
 filenames = glob(argv[1]+'/*')
 
@@ -80,7 +89,7 @@ for filename in filenames:
             sigma = sigma_p(snr, NP)
         
         with open(argv[1] + "/sigma.txt", "at") as fp:
-            fp.write("%s , %f\n"%(basename(filename),sigma))
+            fp.write("%s , %d , %f , %f\n"%(basename(filename),NP,snr,sigma))
     except Exception as e:
         continue
 
